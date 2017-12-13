256a758085a017fbcd83908a9c2ac67590ac5789
Martyna <502285832@logon.ds.ge.com>
2017-12-13 20:37:06 +0100

    files added

---
+++ src/main/java/pl/coderstrust/db/impl/file/FileHelper.java
@@ -0,0 +1,72 @@
+package pl.coderstrust.db.impl.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class FileHelper {
+  
+  
+  void appendFile(File file, String string) {
+    try (PrintWriter outputStream = new PrintWriter(
+        new FileOutputStream(file, true))) {
+      outputStream.println(string);
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    }
+  }
+  
+  void overwriteFile(File file, String stringList) {
+    try (PrintWriter outputStream = new PrintWriter(
+        new FileOutputStream(file, false))) {
+      outputStream.println(stringList);
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    }
+  }
+  
+  boolean clearFile(File file) {
+    boolean fileCleared = false;
+    try {
+      PrintWriter printWriter = new PrintWriter(file.getPath());
+      printWriter.close();
+      fileCleared = true;
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return fileCleared;
+  }
+  
+  List<byte[]> readAsListOfByteArray(File file) {
+    List<byte[]> listOfBytes = new ArrayList<>();
+    try {
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        listOfBytes.add(scanner.nextLine().getBytes());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return listOfBytes;
+  }
+  
+  List<String> readAsStringList(File file) {
+    List<String> stringList = new ArrayList<>();
+    try {
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        stringList.add(scanner.nextLine());
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+    return stringList;
+  }
+  
+  
+}
---
+++ src/main/java/pl/coderstrust/db/impl/file/InFile.java
@@ -0,0 +1,99 @@
+package pl.coderstrust.db.impl.file;
+
+import pl.coderstrust.db.Database;
+import pl.coderstrust.model.Invoice;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class InFile implements Database {
+  
+  private String defaultInFileName = "InFile-Invoices" + LocalDate.now().getYear();
+  private File inFileDb = new File(defaultInFileName);
+  
+  private String inFileAssistingName = defaultInFileName + "_id";
+  private File inFileId = new File(inFileAssistingName);
+  
+  private final AtomicReference<Integer> invoiceId = new AtomicReference<>(0);
+  
+  private FileHelper fileHelper = new FileHelper();
+  private JsonConverter jsonConverter = new JsonConverter();
+  
+  
+  public InFile() {
+  }
+  
+  
+  @Override
+  public Integer getNextInvoiceId() {
+    Integer nextId;
+  
+    if (inFileId.exists()) {
+      String id = (fileHelper.readAsStringList(inFileId).get(0));
+      nextId = Integer.parseInt(id);
+    } else {
+      nextId = invoiceId.get();
+    }
+    nextId++;
+    return nextId;
+  }
+  
+  @Override
+  public boolean saveInvoice(Invoice invoice) {
+    String invoiceAsString = jsonConverter.objectToJson(invoice);
+    fileHelper.appendFile(inFileDb, invoiceAsString);
+  
+    String idAsString = jsonConverter.objectToJson(invoice.getInvoiceId());
+    fileHelper.overwriteFile(inFileId, idAsString);
+    return true;
+  }
+  
+  @Override
+  public Invoice getInvoice(Integer invoiceId) {
+    Invoice invoice = null;
+    List<String> stringList = fileHelper.readAsStringList(inFileDb);
+    for (String string : stringList) {
+      Invoice candidate = jsonConverter.jsonStringToInvoice(string);
+      if (candidate.getInvoiceId().equals(invoiceId)) {
+        invoice = candidate;
+        break;
+      }
+    }
+    return invoice;
+  }
+  
+  @Override
+  public List<Invoice> getAllInvoices() {
+    List<String> stringList = fileHelper.readAsStringList(inFileDb);
+    List<Invoice> invoices = new ArrayList<>();
+    for (String string : stringList) {
+      Invoice invoice = jsonConverter.jsonStringToInvoice(string);
+      invoices.add(invoice);
+    }
+    return invoices;
+  }
+  
+  @Override
+  public boolean removeInvoice(Integer invoiceId) {
+    List<String> inputStringList = fileHelper.readAsStringList(inFileDb);
+    List<String> outputStringList = new ArrayList<>();
+    boolean invoiceDeleted = false;
+  
+    for (String string : inputStringList) {
+      Invoice candidate = jsonConverter.jsonStringToInvoice(string);
+      if (candidate.getInvoiceId().equals(invoiceId)) {
+        invoiceDeleted = true;
+      } else {
+        outputStringList.add(string);
+      }
+    }
+    if (invoiceDeleted) {
+      fileHelper.clearFile(inFileDb);
+      outputStringList.forEach(s -> fileHelper.appendFile(inFileDb, s));
+    }
+    return invoiceDeleted;
+  }
+}
