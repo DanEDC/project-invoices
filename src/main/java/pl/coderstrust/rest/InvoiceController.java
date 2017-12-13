f489b4539e6bb7022a13482b39d39820afcb2328
DanEDC <kursantjava48@gmail.com>
2017-12-13 20:56:29 +0100

    Added Beans and Object Mapper. Started first tests

--- src/main/java/pl/coderstrust/rest/InvoiceController.java
+++ src/main/java/pl/coderstrust/rest/InvoiceController.java
@@ -1,30 +1,48 @@
 package pl.coderstrust.rest;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.coderstrust.db.impl.file.InFile;
 import pl.coderstrust.logic.InvoiceBook;
 import pl.coderstrust.model.Invoice;
 
-import java.util.List;
-
-
+//OD MATEUSZA:
+//@RestController
+//@RequestMapping("/invoices")
+//public class InvoiceController {
+//  /*service here!!*///TODO
+//  InvoiceBook invoiceBook = new InvoiceBook(new InFile());
+//
+//  @GetMapping
+//  public List<Invoice> getInvoices() {
+//    return invoiceBook.getAllInvoices();
+//  }
+//
+//  @GetMapping(value = "/{id}")
+//  public Invoice getInvoice(@PathVariable int id) {
+//    return invoiceBook.getInvoice(id);
+//  }
 @RestController
-@RequestMapping("/invoices")
 public class InvoiceController {
-  /*service here!!*///TODO
-  InvoiceBook invoiceBook = new InvoiceBook(new InFile());
-  
-  @GetMapping
+
+  private InvoiceBook invoiceBook;
+
+  @Autowired
+  public InvoiceController( InvoiceBook invoiceBook) {
+    this.invoiceBook = invoiceBook;
+  }
+
+  @RequestMapping("/invoices")
   public List<Invoice> getInvoices() {
     return invoiceBook.getAllInvoices();
   }
-  
+
   @GetMapping(value = "/{id}")
   public Invoice getInvoice(@PathVariable int id) {
     return invoiceBook.getInvoice(id);
+
   }
-  
 }
