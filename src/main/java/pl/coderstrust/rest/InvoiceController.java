35e233298ca839428aa19208016a18b6df225d99
MatDls <mdzielinski@gmail.com>
2017-12-14 13:43:20 +0100

    Autowured doesn't work

--- src/main/java/pl/coderstrust/rest/InvoiceController.java
+++ src/main/java/pl/coderstrust/rest/InvoiceController.java
@@ -1,48 +1,37 @@
 package pl.coderstrust.rest;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderstrust.db.impl.file.InFile;
 import pl.coderstrust.logic.InvoiceBook;
 import pl.coderstrust.model.Invoice;
 
-//OD MATEUSZA:
-//@RestController
-//@RequestMapping("/invoices")
-//public class InvoiceController {
-//  /*service here!!*///TODO
-//  InvoiceBook invoiceBook = new InvoiceBook(new InFile());
-//
-//  @GetMapping
-//  public List<Invoice> getInvoices() {
-//    return invoiceBook.getAllInvoices();
-//  }
-//
-//  @GetMapping(value = "/{id}")
-//  public Invoice getInvoice(@PathVariable int id) {
-//    return invoiceBook.getInvoice(id);
-//  }
+import java.util.List;
+
+
 @RestController
 public class InvoiceController {
+  
+  InvoiceBook invoiceBook = new InvoiceBook(new InFile());
 
-  private InvoiceBook invoiceBook;
-
-  @Autowired
-  public InvoiceController( InvoiceBook invoiceBook) {
-    this.invoiceBook = invoiceBook;
-  }
-
+//  @Autowired//TODO
+//  public InvoiceController(InvoiceBook invoiceBook) {
+//    this.invoiceBook = invoiceBook;
+//  }
+  
   @RequestMapping("/invoices")
+  @GetMapping
   public List<Invoice> getInvoices() {
     return invoiceBook.getAllInvoices();
   }
-
-  @GetMapping(value = "/{id}")
+  
+  @RequestMapping(value = "/invoices/{id}")
+  @GetMapping
   public Invoice getInvoice(@PathVariable int id) {
     return invoiceBook.getInvoice(id);
-
   }
+  
 }
+
