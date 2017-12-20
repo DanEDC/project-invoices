39363f55fea67a592cd9845f3a1d13ab3ffac246
DanEDC <kursantjava48@gmail.com>
2017-12-20 17:03:15 +0100

    Added Swagger configurator class, GET and POST are working, DELETE isn't.

--- src/main/java/pl/coderstrust/rest/InvoiceController.java
+++ src/main/java/pl/coderstrust/rest/InvoiceController.java
@@ -1,14 +1,18 @@
 package pl.coderstrust.rest;
 
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderstrust.logic.InvoiceBook;
 import pl.coderstrust.model.Invoice;
 
 import java.util.List;
 
+@Api(value = "/invoices", description = "Operations on invoices")
 
 @RestController
 public class InvoiceController {
@@ -29,6 +33,17 @@ public class InvoiceController {
   @GetMapping(value = "/invoices/{id}")
   public Invoice getInvoice(@PathVariable int id) {
     return invoiceBook.getInvoice(id);
+  }
+
+  @PostMapping(value = "/invoices")
+  public int saveInvoice(Invoice invoice) {
+    return 0;
+  }
+
+  //NOT working, in development
+  @DeleteMapping(value = "/invoices{id}")
+  public int removeInvoice(@PathVariable int id) {
+    return 0;
   }
 
 }
