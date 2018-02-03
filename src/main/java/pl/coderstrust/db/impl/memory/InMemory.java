6d6fa4e6ab36025ffe90f9ac253bc00dca48b013
MatDls <mdzielinski@gmail.com>
2018-02-03 13:05:26 +0100

    InMemory logs corrected

--- src/main/java/pl/coderstrust/db/impl/memory/InMemory.java
+++ src/main/java/pl/coderstrust/db/impl/memory/InMemory.java
@@ -27,19 +27,16 @@ public class InMemory implements Database {
   
   @Override
   public final Integer getNextInvoiceId() {
-    logger.debug("getNextInvoiceId called");
     return ++invoiceId;
   }
-
+  
   @Override
   public boolean saveInvoice(Invoice invoice) {
-    logger.debug("saveInvoice called");
     return database.add(invoice);
   }
 
   @Override
   public Invoice getInvoiceById(Integer invoiceId) {
-    logger.debug("getInvoiceById called");
     Invoice invoice = null;
     for (Invoice candidate : database) {
       if (candidate.getInvoiceId().equals(invoiceId)) {
@@ -52,7 +49,6 @@ public class InMemory implements Database {
   
   @Override
   public List<Invoice> getInvoicesFromDateToDate(LocalDate from, LocalDate to) {
-    logger.debug("getInvoicesFromDateToDate called");
     List<Invoice> invoices = new ArrayList<>();
     for (Invoice invoice : database) {
       if (invoice.compareTo(from) >= 0 && invoice.compareTo(to) <= 0) {
@@ -64,20 +60,17 @@ public class InMemory implements Database {
   
   @Override
   public List<Invoice> getAllInvoices() {
-    logger.debug("getAllInvoices called");
     return database;
   }
 
   @Override
   public Invoice removeInvoiceById(Integer invoiceId) {
-    logger.debug("removeInvoiceById called");
     int index = database.indexOf(this.getInvoiceById(invoiceId));
     return database.remove(index);
   }
   
   @Override
   public List<Invoice> removeAllInvoices() {
-    logger.debug("removeAllInvoices called");
     List<Integer> idList = this.getAllIds();
     List<Invoice> removedInvoices = new ArrayList<>();
     idList.forEach(integer -> removedInvoices.add(this.removeInvoiceById(integer)));
@@ -86,7 +79,6 @@ public class InMemory implements Database {
   
   @Override
   public List<Integer> getAllIds() {
-    logger.debug("getAllIds called");
     List<Integer> idList = new ArrayList<>();
     database.forEach(invoice -> idList.add(invoice.getInvoiceId()));
     Collections.sort(idList);
@@ -95,22 +87,18 @@ public class InMemory implements Database {
   
   @Override
   public boolean dropDatabase() {
-    logger.debug("dropDatabase called");
     this.removeAllInvoices();
     return this.getAllInvoices().size() == 0;
   }
   
-  
   @Override
   public String toString() {
-    logger.debug("toString called");
     return "InMemory\tdatabase:\n"
         + database;
   }
 
   @Override
   public boolean equals(Object object) {
-    logger.debug("equals called");
     if (this == object) {
       return true;
     }
@@ -129,7 +117,6 @@ public class InMemory implements Database {
 
   @Override
   public int hashCode() {
-    logger.debug("hashCode called");
     int result = database != null ? database.hashCode() : 0;
     result = 31 * result + (invoiceId != null ? invoiceId.hashCode() : 0);
     return result;
